8cbdf87b52c1a51f8537bd55b73c912af57b6a3e
Ermakovich <dimontiranid1999@gmail.com>
2016-10-07 22:23:20 +0300

    First commit. Interface Cards and class Card

---
+++ Card.java
@@ -0,0 +1,80 @@
+
+import jdk.nashorn.internal.objects.annotations.Constructor;
+
+import java.util.*;
+
+/**
+ * @author Dmitry Ermakovich
+ */
+
+interface Cards {
+    enum  SUITS {Heart, Diamond, Club, Spade}
+
+    HashMap<String, Integer> cardsvalue = new HashMap<>();
+
+    SUITS getSuit();
+    String getFace();
+    Integer getScore();
+
+
+
+}
+
+public class Card implements Cards {
+    private static Random random = new Random();
+    private SUITS suit;
+    private String face;
+    private Integer score;
+
+    public SUITS getSuit() {
+        return suit;
+    }
+
+    public String getFace() {
+        return face;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    private void CardsvalueFiller() {
+        cardsvalue.put("2", 2);
+        cardsvalue.put("3", 3);
+        cardsvalue.put("4", 4);
+        cardsvalue.put("5", 5);
+        cardsvalue.put("6", 6);
+        cardsvalue.put("7", 7);
+        cardsvalue.put("8", 8);
+        cardsvalue.put("9", 9);
+        cardsvalue.put("10", 10);
+        cardsvalue.put("Jack", 10);
+        cardsvalue.put("Queen", 10);
+        cardsvalue.put("King", 10);
+        cardsvalue.put("Ace", 11);
+    }
+
+    // Constructor
+    public Card() {
+        CardsvalueFiller();
+
+        suit = SUITS.values()[random.nextInt(4)];
+        face = (String) cardsvalue.keySet().toArray()[random.nextInt(13)];
+        score = cardsvalue.get(face);
+    }
+
+
+
+    public static void main(String[] args) {
+        Cards card = new Card();
+        System.out.println
+                ("face " +
+                card.getFace() + "\nsuit " +
+                card.getSuit() + "\nscore " +
+                card.getScore()
+                );
+
+        System.out.println(new Card().getFace());
+
+    }
+}
