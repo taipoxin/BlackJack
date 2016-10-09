63740f0c9130158e2b59d35bd84ce8fba084aefe
tiranid99 <dimontiranid1999@gmail.com>
2016-10-09 10:57:43 +0300

    Fix some bugs. now you can play it normally

--- BlackJackMain.java
+++ BlackJackMain.java
@@ -8,190 +8,226 @@ public class BlackJackMain {
     // Start Game
     private static void Game() {
 
-    while (Main.lifecycle) {
-        // Dealer (Singleton)
-        Dealer inst = Dealer.INSTANCE;
+        while (Main.lifecycle) {
 
-        // Console input
-        Scanner sc = new Scanner(System.in);
-        int playersCount = Main.playersCount;
+            // Dealer (Singleton)
+            Dealer inst = Dealer.INSTANCE;
+            Main.deletePlayers();
 
-        if (Main.playersCount == 0) {
-
-            System.out.println("Enter  count of players");
-            Main.playersCount = sc.nextByte();
-            // чтобы все время не обращаться в поле Main.playersCount
-            playersCount = Main.playersCount;
-            sc.skip("\n");
-
-
-            String name;
-
-            // Enter players
-            for (int i = 0; i < playersCount; i++) {
-                System.out.println("Enter player name");
-                name = sc.nextLine();
-                // create new Player
-                Main.players[i] = new Player(name);
+            if (Main.canPlay == 0 & Main.players[0] != null) {
+                System.out.println("Nobody can play");
+                break;
             }
-        }
 
-        // Bets
-        System.out.println("\nBets:");
-        for (int i = 0; i < playersCount; i++) {
-            System.out.println("Hello, " + Main.players[i].name + ". How much money you would like to bet?");
-            Main.players[i].Bet(sc.nextInt());
-            System.out.println("Nice, you bet " + Main.players[i].bet + "$");
+            // Console input
+            Scanner sc = new Scanner(System.in);
+            int playersCount = Main.playersCount;
 
-        }
-        System.out.println("All bets are accepted");
-        // 1 card for dealer
-        // ATTENTION! It's important to .takeCard() go before .giveCard()
-        inst.takeCard();
+            if (Main.playersCount == 0) {
 
-        // Dealer give all 2 cards, and 1 for himself
-        for (int i = 0; i < playersCount; i++) {
-            inst.giveCard(Main.players[i]);
-            inst.giveCard(Main.players[i]);
-        }
+                System.out.println("Enter  count of players");
+                Main.playersCount = sc.nextByte();
+                // чтобы все время не обращаться в поле Main.playersCount
+                playersCount = Main.playersCount;
+                sc.skip("\n");
 
 
-        // show all statistics.
-        Main.showCards();
+                String name;
 
-        // // TODO: 08.10.16 there all need to test
-        for (int i = 0; i < playersCount; i++) {
-            if (Main.players[i].score < 20) {
+                // Enter players
+                for (int i = 0; i < playersCount; i++) {
+                    System.out.println("Enter player name");
+                    name = sc.nextLine();
+                    // create new Player
+                    Main.players[i] = new Player(name);
+                }
+            }
+            Main.showBank();
 
-                System.out.println("statistic: ");
-                // может иногда случаться лишний вывод
+            // Bets
+            System.out.println("\nBets:");
+            for (int i = 0; i < playersCount; i++) {
+                if (!Main.players[i].bkr) {
+
+                    do {
+                        System.out.println("Hello, " + Main.players[i].name + ". How much money you would like to bet?");
+                        Main.players[i].Bet(sc.nextInt());
+                    }
+                    while (Main.players[i].bet > Main.players[i].money | Main.players[i].bet <= 0);
+                    Main.players[i].money -= Main.players[i].bet;
+
+                    System.out.println("Nice, you bet " + Main.players[i].bet + "$");
+                }
+
+            }
+            System.out.println("All bets are accepted\n");
+            // 1 card for dealer
+            // ATTENTION! It's important to .takeCard() go before .giveCard()
+            inst.takeCard();
+
+            // Dealer give all 2 cards, and 1 for himself
+            for (int i = 0; i < playersCount; i++) {
+                inst.giveCard(Main.players[i]);
+                inst.giveCard(Main.players[i]);
+            }
+
+
+            // show all statistics.
+            // protect double showStatistics
+            if (Main.players[0].score > 19)
                 Main.showCards();
-                System.out.println("Hello, " + Main.players[i].name + " , now you can take card");
-                System.out.println("Now you can take one more card(Y), or no(N)");
-                String s = "";
-                while ((!s.equals("N") | Main.players[i].score < 20) & (Main.players[i].cardsCount < 6)) {
-                    while (!s.equals("Y") & !s.equals("N")) {
-                        s = sc.nextLine();
-                        s = s.toUpperCase();
-                    }
-                    if (s.equals("N")) {
-                        System.out.println("OK");
-                        s = "";
-                        break;
-                    }
-                    if (s.equals("Y")) {
-                        inst.giveCard(Main.players[i]);
-                        Main.showCards();
-                        if (!(Main.players[i].score < 20)) {
+
+            // // TODO: 08.10.16 there all need to test
+            for (int i = 0; i < playersCount; i++) {
+                if (Main.players[i].score < 20 & !Main.players[i].bkr) {
+
+                    System.out.println("statistic: ");
+                    Main.showCards();
+                    System.out.println("Hello, " + Main.players[i].name + " , now you can take card");
+                    System.out.println("Now you can take one more card(Y), or no(N)");
+                    String s = "";
+                    while ((!s.equals("N") | Main.players[i].score < 20) & (Main.players[i].cardsCount < 5)) {
+                        while (!s.equals("Y") & !s.equals("N")) {
+                            s = sc.nextLine();
+                            s = s.toUpperCase();
+                        }
+                        if (s.equals("N")) {
+                            System.out.println("OK");
                             s = "";
                             break;
                         }
+                        if (s.equals("Y")) {
+                            inst.giveCard(Main.players[i]);
+                            Main.showCards();
+                            if (!(Main.players[i].score < 20)) {
+                                s = "";
+                                break;
+                            }
 
-                        System.out.println("Take another card?");
-                        s = "";
+                            System.out.println("Take another card?");
+                            s = "";
+                        }
                     }
+                } else {
+                    System.out.println("Hi, " + Main.players[i].name + " , you already have enough score");
                 }
-            } else {
-                System.out.println("Hi, " + Main.players[i].name + " , you already have enough score");
             }
-        }
 
-        // Dealer
-        while (inst.score < 17 & inst.cardsCount < 6) {
-            inst.takeCard();
-            System.out.println("Dealer take card: ");
-            System.out.println("Dealer score is: " + inst.score);
-        }
-        Main.showCards();
+            // Dealer
+            while (inst.score < 17 & inst.cardsCount < 5) {
+                inst.takeCard();
+                System.out.println("Dealer take card: ");
+                System.out.println("Dealer score is: " + inst.score);
+            }
+            Main.showCards();
 
 
-        // Results
-        for (int i = 0; i < playersCount; i++) {
-            Player p = Main.players[i];
+            // Results
+            for (int i = 0; i < playersCount; i++) {
+                Player p = Main.players[i];
 
-            switch (p.bj) {
-                case GOOD_BLACKJACK: {
-                    p.money += 2 * p.bet;
-                    p.bet = 0;
-                    continue;
-                }
-                case SAFE_BLACKJACK: {
-                    p.money += 2 * p.bet;
-                    p.bet = 0;
-                    continue;
-                }
-                case BLACKJACK: {
-                    if (inst.score != 21) {
-                        p.money += 2.5 * p.bet;
-                        p.bet = 0;
-                        continue;
-                    } else {
-                        p.money += p.bet;
-                        p.bet = 0;
-                        continue;
-                    }
-                }
-                case NULL: {
-                    if (p.score < 22 && inst.score > 21) {
+                switch (p.bj) {
+                    case GOOD_BLACKJACK: {
                         p.money += 2 * p.bet;
                         p.bet = 0;
                         continue;
                     }
-
-
-                    if (p.score < 22 && inst.score < 22) {
-                        if (p.score > inst.score) {
+                    case SAFE_BLACKJACK: {
+                        p.money += 2 * p.bet;
+                        p.bet = 0;
+                        continue;
+                    }
+                    case BLACKJACK: {
+                        if (inst.score != 21) {
+                            p.money += 2.5 * p.bet;
+                            p.bet = 0;
+                            continue;
+                        } else {
+                            p.money += p.bet;
+                            p.bet = 0;
+                            continue;
+                        }
+                    }
+                    case NULL: {
+                        if (p.score < 22 && inst.score > 21) {
                             p.money += 2 * p.bet;
                             p.bet = 0;
                             continue;
                         }
-                        if (p.score == inst.score) {
-                            p.money += p.bet;
-                            p.bet = 0;
-                            continue;
-                        } else {
+
+
+                        if (p.score < 22 && inst.score < 22) {
+                            if (p.score > inst.score) {
+                                p.money += 2 * p.bet;
+                                p.bet = 0;
+                                continue;
+                            }
+                            if (p.score == inst.score) {
+                                p.money += p.bet;
+                                p.bet = 0;
+                                continue;
+                            } else {
+                                p.bet = 0;
+                                continue;
+                            }
+                        } else if (p.score > 21 && inst.score < 22) {
                             p.bet = 0;
                             continue;
                         }
-                    } else if (p.score > 21 && inst.score < 22) {
+                    }   // end case
+                }   // end switch
+                p.bet = 0;
+            }   // end for
+
+            System.out.println("GG");
+
+            System.out.println();
+            // метод вывода банков, ставок всех игроков.
+            Main.showBank();
+
+            //
+            System.out.println("Now we can: ");
+            System.out.println("1:\t End game");
+            System.out.println("2:\t Restart game with old players");
+            System.out.println("3:\t Change players and play");
+            System.out.println("What are you choose?");
+            int choice = 0;
+            while (choice != 1 & choice != 2 & choice != 3)
+                choice = sc.nextByte();
+
+            switch (choice) {
+                case 1: {
+                    Main.lifecycle = false;
+                    break;
+                }
+                case 2: {
+                    for (int i = 0; i < playersCount; i++) {
+                        Player p = Main.players[i];
+                        p.hand = new Card[5];
+                        p.score = 0;
                         p.bet = 0;
-                        continue;
+                        p.aces = 0;
+                        p.cardsCount = 0;
+                        p.bj = Player.BlackJack.NULL;
+
                     }
-                }   // end case
-            }   // end switch
-        }   // end for
-
-        System.out.println("GG");
-
-        System.out.println();
-        // метод вывода банков, ставок всех игроков.
-
-        //
-        System.out.println("Now we can: ");
-        System.out.println("1:\t End game");
-        System.out.println("2:\t Restart game with old players");
-        System.out.println("3:\t Change players and play");
-        System.out.println("What are you choose?");
-        int choice = 0;
-        while (choice != 1 & choice != 2 & choice != 3)
-            choice = sc.nextByte();
-
-        switch (choice) {
-            case 1: {
-                Main.lifecycle = false;
-                break;
-            }
-            case 2: {
-                break;
-            }
-            case 3: {
-                Main.players = new Player[6];
-                Main.playersCount = 0;
-                break;
+                    Dealer.INSTANCE.dealerHand = new Card[5];
+                    Dealer.INSTANCE.cardsCount = 0;
+                    Dealer.INSTANCE.score = 0;
+                    Dealer.INSTANCE.aces = 0;
+                    break;
+                }
+                case 3: {
+                    Main.players = new Player[6];
+                    Main.playersCount = 0;
+                    Dealer.INSTANCE.dealerHand = new Card[5];
+                    Dealer.INSTANCE.cardsCount = 0;
+                    Dealer.INSTANCE.score = 0;
+                    Dealer.INSTANCE.aces = 0;
+                    break;
+                }
             }
         }
-
-    }
 
     }
 }
--- Main.java
+++ Main.java
@@ -6,26 +6,54 @@ class Main {
     static Player[] players = new Player[6];
     static byte playersCount = 0;
     static boolean lifecycle = true;
+    static byte canPlay = 0;
+
+    static void deletePlayers() {
+        for (int i = 0; i < playersCount; i++) {
+            if (players[i].money <= 0) {
+                System.out.println("Player " + players[i].name + " hasn't money");
+                players[i].bkr = true;
+            }
+            else {
+                canPlay++;
+            }
+        }
+    }
+
+    /**
+     * This method show fields "money" and current bets
+     */
+    static void showBank() {
+        for (int i = 0; i < playersCount; i++) {
+            Player p = players[i];
+            System.out.println(p.name + ": ");
+            System.out.println("\t current bet: " + p.bet);
+            System.out.println("\t current bank: " + p.money + "\n");
+        }
+    }
 
 
     static void showCards() {
         for (int i = 0; i < playersCount; i++) {
             Player p = players[i];
-            System.out.println(p.name + ": ");
+            if (!p.bkr) {
+                System.out.println(p.name + ": ");
 
-            for (int card = 0; card < p.cardsCount; card++) {
-                Card currentCard = p.hand[card];
-                System.out.println(currentCard.getSuit() + " " + currentCard.getFace() );
+                for (int card = 0; card < p.cardsCount; card++) {
+                    Card currentCard = p.hand[card];
+                    System.out.println(currentCard.getSuit() + " " + currentCard.getFace());
+                }
+                System.out.print("score: ");
+
+                // KOSTILI POLETELI
+                if (p.aces != 0 && p.cardsCount < 3)
+                    System.out.println(p.score + " (" + (p.score - (p.aces * 10)) + ")");
+
+                else
+                    System.out.println(p.score);
+
+                System.out.println();
             }
-            System.out.print("score: ");
-
-            if (p.aces != 0 && p.score < 21)
-                System.out.println(p.score + " (" + (p.score - (p.aces * 10)) + ")");
-
-            else
-                System.out.println(p.score);
-
-            System.out.println();
         }
 
 
